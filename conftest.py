9c50d704ab0fe2e491c8cf1ed3193cbae8aa9782
KoksharovVV <vladimircoksharoff@yandex.ru>
2024-08-17 17:08:30 +0500

    Create structure

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager as cd
+from webdriver_manager.firefox import GeckoDriverManager as fd
+from selenium.webdriver.chrome.service import Service as cs
+from selenium.webdriver.firefox.service import Service as fs
+
+
+@pytest.fixture(params=['chrome'])
+def driver(request):
+    if request.param == 'firefox':
+        options = webdriver.FirefoxOptions()
+        service = fs(executable_path=fd().install())
+        options.add_argument('--window-size=1920,1080')
+        driver = webdriver.Firefox(service=service, options=options)
+    if request.param == 'chrome':
+        options = webdriver.ChromeOptions()
+        service = cs(executable_path=cd().install())
+        options.add_argument('--window-size=1920,1080')
+        driver = webdriver.Firefox(service=service, options=options)
+    yield driver
+    driver.quit()
---
+++ data.py
@@ -0,0 +1,4 @@
+class TestDataUrl:
+    MAIN_URL = "https://stellarburgers.nomoreparties.site/"
+    LOGIN_PAGE_URL = "https://stellarburgers.nomoreparties.site/login"
+    PASSWORD_RECOVERY_PAGE_URL = "https://stellarburgers.nomoreparties.site/forgot-password"
